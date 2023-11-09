a54625170bc9ed85653c256990204884ae86fb5a
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-09 12:34:55 +0100

    Ajoutez des fichiers projet.

---
+++ ASPMVC/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok("Bienvenu dans la démo!");
+        }
+
+        [Route("DemoRouting")]
+        [Route("Demo/Routing")]
+        [Route("Demo/RoutingAttribute")]
+        public IActionResult RoutingAttribute()
+        {
+            return Ok("J'ai atteint /Demo/RoutingAttribute à l'aide d'un RouteAttribute");
+        }
+    }
+}
